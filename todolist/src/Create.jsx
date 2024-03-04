c7a9a21dece00141ae8482df693e92b12b2a694f
yarden8090 <Yardenben999@gmail.com>
2024-03-04 20:20:39 +0200

    first commit

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+function Create(){
+    const [task, setTask] = useState()
+    const handleAdd = () =>{
+        axios.post('http://localhost:3001/add', {task: task}) //route to add task
+        .then(result => {
+            location.reload()
+        })
+        .catch(err => console.log(err))
+        }
+    return(
+        <div className='create_from'>
+            <input type="text" placeholder='Enter Task' onChange={(e)=> setTask(e.target.value)}/>
+            <button type="button" onClick={handleAdd}>Add</button>
+        </div>
+    )
+}
+export default Create
